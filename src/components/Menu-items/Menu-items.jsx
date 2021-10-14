ee55cc5819d4e3a17db5c22487ea5a408b1d6eda
Mahdi A <mahdi.alizadeh.sp@gmail.com>
2021-10-14 11:18:33 +0330

    home page
    home page component done.
    home page styles done.

---
+++ src/components/Menu-items/Menu-items.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MenuItems({title, imageUrl , size}) {
+    return (
+        <div className={`${size} menu-item`} >
+            <div className={'background-image'} style={{ backgroundImage : `url(${imageUrl})`}}>
+            </div>
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+    );
+};
