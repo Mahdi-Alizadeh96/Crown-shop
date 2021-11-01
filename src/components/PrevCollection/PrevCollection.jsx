9a5995e0feb37bd53abab50b0f358f36d60f5cfb
Mahdi A <mahdi.alizadeh.sp@gmail.com>
2021-11-01 14:33:26 +0330

    collection item alfa

--- src/components/PrevCollection/PrevCollection.jsx
+++ src/components/PrevCollection/PrevCollection.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import CollectionItem from '../Collection-item/Collection-item';
 
 export default function PrevCollection({title ,items}) {
     return (
@@ -6,8 +7,8 @@ export default function PrevCollection({title ,items}) {
             <h1 className={'title'}>{title.toUpperCase()}</h1>
             <div className={'preview'}>
                 {
-                    items.filter((item,inx)=> inx < 4).map((item) => (
-                        <div key={item.id} >{item.name}</div>
+                    items.filter((item,inx)=> inx < 4).map(({id , ...otherProps}) => (
+                        <CollectionItem key={id} {...otherProps} />
                     ))
                 }
             </div>
